8c99c289524ab401f7fa766028aeeb1e0d452d12
GreYak <GreYak@users.noreply.github.com>
2026-06-06 18:11:04 +0200

    Initialisation BasketService

---
+++ BasketService/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using BasketService.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace BasketService.Controllers
+{
+    [RoutePrefix("api/basket")]
+    public class BasketController : ApiController
+    {
+        private static List<BasketItem> _basket = new List<BasketItem>();
+
+        // GET api/basket
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<BasketItem> Get()
+        {
+            return _basket;
+        }
+
+        // POST api/basket/add
+        [HttpPost]
+        [Route("add")]
+        public IHttpActionResult Add(BasketItem item)
+        {
+            if (item == null)
+                return BadRequest("Item invalide");
+
+            _basket.Add(item);
+            return Ok();
+        }
+
+        // POST api/basket/clear
+        [HttpPost]
+        [Route("clear")]
+        public IHttpActionResult Clear()
+        {
+            _basket.Clear();
+            return Ok();
+        }
+    }
+}
---
+++ BasketService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasketService.WebApiApplication" Language="C#" %>
